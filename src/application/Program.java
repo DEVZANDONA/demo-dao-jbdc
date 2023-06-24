7b321ac7468306672153f1c15d7a13e0a0a6971b
Pedro Zandoná <pedruco.zandona@gmail.com>
2023-06-24 15:58:25 -0300

    Classe Seller Criada

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,18 @@
 package application;
 
+import java.time.LocalDate;
+
 import entities.Department;
+import entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(5,"BOOKS");
+		Seller seller = new Seller(5,"Pedro","pedruco.zandona@gmail.com",LocalDate.now(),3000.00,obj);
 		
-		System.out.println(obj);
+		System.out.println(seller);
 
 	}
 
